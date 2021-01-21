e5e95e5a610265c88a93317d55d91ac3be4351a2
Kristian Bossev <bossev91@gmail.com>
2021-01-21 17:56:59 +0200

    Tasks

---
+++ Sets And Dictionaries Advanced/UniqueUserNames/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SetsAndDictionariesAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                if(!names.Contains(input))
+                {
+                    names.Add(input);
+                }
+            }
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
