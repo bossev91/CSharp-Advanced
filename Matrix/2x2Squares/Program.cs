2a6d2b3273178e55ea88e63445245c2549baa54c
Kristian Bossev <bossev91@gmail.com>
2021-01-21 16:41:41 +0200

    2x2Squares

---
+++ Matrix/2x2Squares/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _2x2Squares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            char[,] matrix = new char[rows, cols];
+            int counter = 0;
+
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] current = Console.ReadLine().Split();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = char.Parse(current[col]);
+                }
+            }
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    if(matrix[i,j] == matrix[i, j + 1]
+                        && matrix[i, j] == matrix[i + 1, j]
+                        && matrix[i, j] == matrix[i + 1, j + 1])
+                    {
+                        counter++;
+                    }
+                }               
+            }
+
+            Console.WriteLine(counter);
+            
+        }
+    }
+}
