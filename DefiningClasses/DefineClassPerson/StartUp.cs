0c48664b3be612c4ff49ea1c5b504a2a230261a1
Kristian Bossev <bossev91@gmail.com>
2021-01-25 15:37:51 +0200

    Tasks

---
+++ DefiningClasses/DefineClassPerson/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person Pesho = new Person("Pesho", 20);
+            Person Gosho = new Person("Gosho", 18);
+            Person Stamat = new Person("Stamat", 43);
+           
+        }
+    }
+}
