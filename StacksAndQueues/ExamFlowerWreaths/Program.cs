ed04b36717089cf5001db3ac4bdf33ff6713bb81
Kristian Bossev <bossev91@gmail.com>
2021-02-08 16:43:07 +0200

    ExamTasks

---
+++ StacksAndQueues/ExamFlowerWreaths/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExamFlowerWreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            int wreath = 0;
+            int temp = 0;
+
+            while (lilies.Count != 0 && roses.Count != 0)
+            {
+                int sum = lilies.Peek() + roses.Peek();
+
+                if (sum == 15)
+                {
+                    wreath++;
+                    lilies.Pop();
+                    roses.Dequeue();
+                }
+                else if (sum > 15)
+                {
+                    lilies.Push(lilies.Pop() - 2);
+                    
+                }
+                else if (sum < 15)
+                {
+                    temp += sum;
+                    lilies.Pop();
+                    roses.Dequeue();
+                }
+            }
+
+            while (temp >= 15)
+            {
+                temp -= 15;
+                wreath++;
+            }
+
+            bool isComplete = wreath >= 5;
+            if (isComplete)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreath} wreaths!");
+            }
+            else
+            {
+                int needed = 5 - wreath;
+                Console.WriteLine($"You didn't make it, you need {needed} wreaths more!");
+            }
+        }
+    }
+}
---
+++ StacksAndQueues/ExamLootbox/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExamLootbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> firstBox = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            Stack<int> secondBox = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+                int totalSum = 0;
+            bool isEpic = false;
+
+            while(true)
+            {
+                if (firstBox.Count == 0 || secondBox.Count == 0)
+                {
+                    break;
+                }
+
+                int sum = firstBox.Peek() + secondBox.Peek();
+
+                if(sum % 2 == 0) // even
+                {
+                    totalSum += sum;
+                    {
+                        if(totalSum >= 100)
+                        {
+                            isEpic = true;
+                        }
+                    }
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                }
+                else
+                {
+                    firstBox.Enqueue(secondBox.Pop());                   // odd
+                }
+
+            }
+            string grade = string.Empty;
+            if(isEpic)
+            {
+                grade = "epic!";
+            }
+            else
+            {
+                grade = "poor...";
+
+            }
+
+            if(firstBox.Count == 0)
+            {
+                Console.WriteLine($"First lootbox is empty");
+                Console.WriteLine($"Your loot was {grade} Value: {totalSum}");
+            }
+            else
+            {
+               
+                    Console.WriteLine($"Second lootbox is empty");
+                    Console.WriteLine($"Your loot was {grade} Value: {totalSum}");
+                
+            }
+        }
+    }
+}
